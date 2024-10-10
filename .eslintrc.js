42bd60d0df09453e3c59d66dc7ca1ae495f307ce
ola-Segun <olaniyanpaul012@gmail.com>
2024-10-10 10:22:31 +0100

    Testing deploy to render/ resolving Eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,20 @@
 module.exports = {
-    rules: {
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },  
+  rules: {
       "@next/next/no-img-element": "off", // If you want to keep using <img> tags
       "react/no-unescaped-entities": "off" // If you want to keep using unescaped entities
     }
